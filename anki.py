75fce62236240f2ef90f88c85b1b8cbfb9744758
Anastasiia_Pyrih <anastasiia.pyrih@refinitiv.com>
2020-10-31 21:55:30 +0200

    loggs

--- anki.py
+++ anki.py
@@ -28,11 +28,14 @@ def ask():
 @app.route('/submit', methods=["POST"])
 def submit():
     uw = request.form.get('name')
+    print(f"user word: {uw}", file=sys.stderr)
     global word
     correct_bool = False
+    print(f"question word: {word}", file=sys.stderr)
     cw = df["german"][df["russ"] == word].item()
     try:
         result_word = df["german"][df["german"] == uw].item()
+        print(f"result word: {result_word}")
         correct_bool = True
     except:
         print(f"correct is: '{cw}' and user pasted: '{uw}'", file=sys.stderr)
